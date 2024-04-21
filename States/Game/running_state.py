65b52a637bde10ed42f2a0b3180dfc2d19803aae
TheMikulka <117767622+TheMikulka@users.noreply.github.com>
2024-04-22 01:36:41 +0200

    New assets for players

--- States/Game/running_state.py
+++ States/Game/running_state.py
@@ -17,11 +17,11 @@ class RunningState(GameState):
     def initialize(self) -> None:
         self.map = Map(self._game.width, self._game.height)
         self.table = Q_table("Q_table.json", 6)
-        # self.player1 = Player(self.map.set_starting_postion(0, 23), 'player_1', PLAYER_1_CONTROLS, (8, 7, 4), 32, 32, 2.2, self.map, self._game.screen)
-        self.ai = RL_agent(self.map.set_starting_postion(0, 24), 'player_1', (8, 7, 4), 32, 32, 2.2, self.map, self._game.screen, 1,self.table)
-        self.ai2 = RL_agent(self.map.set_starting_postion(0, 0), 'player_1', (8, 7, 4), 32, 32, 2.2, self.map, self._game.screen, 2, self.table)
-        self.ai3 = RL_agent(self.map.set_starting_postion(12, 0), 'player_1', (8, 7, 4), 32, 32, 2.2, self.map, self._game.screen, 3, self.table)
-        self.ai4 = RL_agent(self.map.set_starting_postion(12, 24), 'player_1', (8, 7, 4), 32, 32, 2.2, self.map, self._game.screen, 4, self.table)
+        # self.player1 = Player(self.map.set_starting_postion(0, 0), 'player_1', PLAYER_1_CONTROLS, (8, 7, 4), 32, 32, 2.2, self.map, self._game.screen)
+        self.ai = RL_agent(self.map.set_starting_postion(0, 0), 'player_1', (8, 7, 4), 32, 32, 2.2, self.map, self._game.screen, 1,self.table)
+        self.ai2 = RL_agent(self.map.set_starting_postion(0, 24), 'player_4', (8, 7, 4), 32, 32, 2.2, self.map, self._game.screen, 2, self.table)
+        self.ai3 = RL_agent(self.map.set_starting_postion(12, 0), 'player_2', (8, 7, 4), 32, 32, 2.2, self.map, self._game.screen, 3, self.table)
+        self.ai4 = RL_agent(self.map.set_starting_postion(12, 24), 'player_3', (8, 7, 4), 32, 32, 2.2, self.map, self._game.screen, 4, self.table)
 
 
         
@@ -35,13 +35,13 @@ class RunningState(GameState):
                         new_state = PauseState(self._game)
                         new_state.enter_state()
                     if event.key == pygame.K_f:
-                        self._game.fullscreen = not self.__fullscreen
+                        self._game.fullscreen = not self._game.fullscreen
                         if self._game.fullscreen:
                             self.handle_fullscreen()
         
     def update(self, delta_time) -> None:
         self.map.render_map(self._game.screen, delta_time)
-        self._game.draw_text('PLAYER 1', (100 * self._game.width // self._game.width), (20 * self._game.height // self._game.height))
+        # self._game.draw_text('PLAYER 1', (100 * self._game.width // self._game.width), (20 * self._game.height // self._game.height))
         
         self.handle_events()
         
@@ -51,7 +51,7 @@ class RunningState(GameState):
             if player._current_state != player.states['Dying']:
                 count_of_alive += 1
 
-        if count_of_alive == 1:
+        if count_of_alive <= 1:
             all_players_dead = True
             for player in self.map.get_players():
                 if player._current_state != player.states['Dying']:
@@ -60,6 +60,7 @@ class RunningState(GameState):
                     all_players_dead = False
 
             if all_players_dead:
+                self.table.save()
                 self.initialize()
                 return
                 
@@ -68,6 +69,5 @@ class RunningState(GameState):
                 player.update(pressed_keys, delta_time)
             else:
                 player.update(delta_time)
-        self.table.save()
         
         
