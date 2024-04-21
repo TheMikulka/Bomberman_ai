c57c544653e91245fb3708c17ce58a9d9bac250f
TheMikulka <117767622+TheMikulka@users.noreply.github.com>
2024-04-22 01:37:16 +0200

    Edited rewards

--- RL_agent.py
+++ RL_agent.py
@@ -1,6 +1,7 @@
 import time
 import random
 import numpy as np
+import math
 import json
 from Obstacles.crate import Crate
 from Obstacles.wall import Wall
@@ -29,6 +30,7 @@ class RL_agent(AiPlayer):
         self.__died = False
         self.__was_idle = False
         self.table = table
+        self.__last_choice_method = ""
 
     def __get_map_with_players(self):
         map_with_players = []
@@ -50,6 +52,31 @@ class RL_agent(AiPlayer):
             map_with_players[y][x] = type(bomb).__name__
 
         return map_with_players
+    
+    def _calculate_nearest_player(self):
+        players = self._map.get_players()
+        smallest_distance = 1000000000000000000000
+        closest_player = None
+        for player in players:
+            if player != self and player._current_state != player.states['Dying']:
+                distance = math.dist([self.x,self.y],[player.x,player.y])
+                if distance < smallest_distance:
+                    smallest_distance = distance
+                    closest_player = player
+        return closest_player
+    
+    def __determine_direction_nearest_player(self):
+        nearest_player = self._calculate_nearest_player()
+        # if nearest_player is not None:
+        #     print(f"{self._identifier}:{nearest_player._identifier}")
+        if nearest_player is not None:
+            distance_x = abs(self.x - nearest_player.x)
+            distance_y = abs(self.y - nearest_player.y)
+            if distance_x > distance_y:
+                return 'EnemyOnLeft' if self.x > nearest_player.x else 'EnemyOnRight'
+            else:
+                return 'EnemyOnTop' if self.y > nearest_player.y else 'EnemyOnBottom'
+        return 'NoEnemy'
     
     def __get_state(self):
         map_copy = self.__get_map_with_players()        
@@ -73,13 +100,24 @@ class RL_agent(AiPlayer):
         on_top_top = map_copy[y - 2][x] if is_inside_map(x,y-2) else "Wall"
         on_bottom_bottom = map_copy[y + 2][x] if is_inside_map(x,y+2) else "Wall"
 
-        on_center = map_copy[y][x]
+        on_center = map_copy[y][x] 
+
+        nearest_player = self.__determine_direction_nearest_player()
         
-        return f"{on_top_top}_{on_left_top}_{on_top}_{on_right_top}_{on_left_left}_{on_left}_{on_center}_{on_right}_{on_right_right}_{on_left_bottom}_{on_bottom}_{on_right_bottom}_{on_bottom_bottom}"
+        return f"{on_top_top}_{on_left_top}_{on_top}_{on_right_top}_{on_left_left}_{on_left}_{on_center}_{on_right}_{on_right_right}_{on_left_bottom}_{on_bottom}_{on_right_bottom}_{on_bottom_bottom}_{nearest_player}"
         
     def __is_in_radius(self, type) -> int:
-        top_top, left_top, top, right_top, left_left, left, center, right, right_right, left_bottom, bottom, right_bottom,bottom_bottom = self.performed_state.split("_")
+        top_top, left_top, top, right_top, left_left, left, center, right, right_right, left_bottom, bottom, right_bottom,bottom_bottom,nearest_player = self.performed_state.split("_")
         sides = [left,right,top,bottom]
+        count = 0
+        for side in sides:
+            if side == type:
+                count += 1
+        return count
+
+    def __is_in_far_radius(self, type) -> int:
+        top_top, left_top, top, right_top, left_left, left, center, right, right_right, left_bottom, bottom, right_bottom,bottom_bottom,nearest_player = self.performed_state.split("_")
+        sides = [left_left, right_right, top_top, bottom_bottom]
         count = 0
         for side in sides:
             if side == type:
@@ -87,7 +125,7 @@ class RL_agent(AiPlayer):
         return count
     
     def __get_direction(self):
-        top_top, left_top, top, right_top, left_left, left, center, right, right_right, left_bottom, bottom, right_bottom,bottom_bottom = self.performed_state.split("_")
+        top_top, left_top, top, right_top, left_left, left, center, right, right_right, left_bottom, bottom, right_bottom,bottom_bottom,nearest_player = self.performed_state.split("_")
         match self.performed_action:
             case 'UP':
                 return [center,top,top_top]
@@ -99,15 +137,15 @@ class RL_agent(AiPlayer):
                 return [center,right,right_right]
     
     def __get_center(self):
-        top_top, left_top, top, right_top, left_left, left, center, right, right_right, left_bottom, bottom, right_bottom,bottom_bottom = self.performed_state.split("_")
+        top_top, left_top, top, right_top, left_left, left, center, right, right_right, left_bottom, bottom, right_bottom,bottom_bottom,nearest_player = self.performed_state.split("_")
         return center
 
     def __get_corners(self):
-        top_top, left_top, top, right_top, left_left, left, center, right, right_right, left_bottom, bottom, right_bottom,bottom_bottom = self.performed_state.split("_")
+        top_top, left_top, top, right_top, left_left, left, center, right, right_right, left_bottom, bottom, right_bottom,bottom_bottom,nearest_player = self.performed_state.split("_")
         return [left_top,left_bottom,right_top,right_bottom]
     
     def __get_direction_corners(self):
-        top_top, left_top, top, right_top, left_left, left, center, right, right_right, left_bottom, bottom, right_bottom,bottom_bottom = self.performed_state.split("_")
+        top_top, left_top, top, right_top, left_left, left, center, right, right_right, left_bottom, bottom, right_bottom,bottom_bottom,nearest_player = self.performed_state.split("_")
         match self.performed_action:
             case 'UP':
                 return [left_top, right_top]
@@ -117,6 +155,21 @@ class RL_agent(AiPlayer):
                 return [left_top, left_bottom]
             case 'RIGHT':
                 return [right_top, right_bottom]
+    
+    def _where_is_neares_player(self):
+        top_top, left_top, top, right_top, left_left, left, center, right, right_right, left_bottom, bottom, right_bottom,bottom_bottom,nearest_player = self.performed_state.split("_")
+        match nearest_player:
+            case 'EnemyOnLeft':
+                return "LEFT"
+            case 'EnemyOnRight':
+                return "RIGHT"
+            case 'EnemyOnTop':
+                return "UP"
+            case 'EnemyOnBottom':
+                return "DOWN"
+            case 'NoEnemy':
+                return "NoEnemy"
+
 
 
     def __print_state(self,state):
@@ -141,7 +194,7 @@ class RL_agent(AiPlayer):
             if index == 12:
                 out[4].append(s[0])
                 
-        print(f"    {".".join(out[0])}\n  {".".join(out[1])}\n{".".join(out[2])}\n  {".".join(out[3])}\n    {".".join(out[4])}")
+        print(f"    {".".join(out[0])}\n  {".".join(out[1])}\n{".".join(out[2])}\t {states[13]}\n  {".".join(out[3])}\n    {".".join(out[4])}")
         
 
 
@@ -167,7 +220,8 @@ class RL_agent(AiPlayer):
         # reward for placing bomb next to crate or player ----------------------> 10
         # walking into tile ----------------------------------------------------> 10
         # walking into player --------------------------------------------------> 10
-        # walking out of bomb
+        # walking out of bomb --------------------------------------------------> 10
+        # walking into player
 
         # -
         # walking into wall or crate ------------------------------------------> -5
@@ -187,8 +241,8 @@ class RL_agent(AiPlayer):
             if close in ["Wall", "Crate"]:
                 total_reward.append(("walk to wall/crate", -5))
 
-            if far in ["Crate", "Player", "Tile"] and close in ["Tile"] and self.__is_in_radius("Bomb") == 0 and center!="Bomb":
-                total_reward.append(("walk into far crate/player/tile",5))
+            if far in ["Crate", "Player"] and close in ["Tile"] and self.__is_in_radius("Bomb") == 0 and center!="Bomb":
+                total_reward.append(("walk into far crate/player",5))
 
             if center == "Bomb" and close == "Tile":
                 if far == "Tile":
@@ -203,10 +257,14 @@ class RL_agent(AiPlayer):
             if center == "Tile" and (close == "Bomb" or far == "Bomb"):
                 total_reward.append(("walk into explosion",-15))
             if "Bomb" in self.__get_direction_corners():
-                total_reward.append(("walk from corner bomb to explosion",-15))
+                total_reward.append(("walk from corner bomb to explosion",-20))
             if self.__is_in_radius("Bomb") != 0 and close == "Tile":
                 total_reward.append(("walk away from bomb", 10))
+            
+            #---------------------------------WALKING_TO_ENEMY---------------------------------
 
+            if self.performed_action == self._where_is_neares_player() and "Bomb" not in self.__get_direction_corners() and self.__is_in_radius("Bomb") == 0 and center != "Bomb" and close == "Tile":
+                total_reward.append(("Walking to enemy",10))
                    
         #---------------------------------PLACING BOMB---------------------------------
         count_crate = self.__is_in_radius("Crate")
@@ -225,22 +283,18 @@ class RL_agent(AiPlayer):
 
         #---------------------------------IDLE---------------------------------
         count_bomb = self.__is_in_radius("Bomb")
+        count_far_bomb = self.__is_in_far_radius("Bomb")
+        center_idle = self.__get_center()
         if self.performed_action == 'IDLE' and count_bomb > 0:
             total_reward.append(("Standing next to bomb",-10))
-
-        if self.performed_action == 'IDLE' and "Bomb" in self.__get_corners():
+        elif self.performed_action == 'IDLE' and "Bomb" in self.__get_corners():
             total_reward.append(("standing in corners",10))
-
-        center_idle = self.__get_center()
-        if self.performed_action == 'IDLE' and center_idle == "Bomb":
+        elif self.performed_action == 'IDLE' and center_idle == "Bomb":
             total_reward.append(("Standing in bomb",-10))
-
-        if self.performed_action != 'IDLE':
-            self.__was_idle = False
-        if self.performed_action == 'IDLE' and self.__was_idle:
-            total_reward.append(("Idling",-10))
-        if self.performed_action == 'IDLE' and not self.__was_idle:
-            self.__was_idle = True
+        elif self.performed_action == 'IDLE' and count_far_bomb > 0:
+            total_reward.append(("Standing in far bomb",10))
+        elif self.performed_action == 'IDLE':
+            total_reward.append(("Idling",-5))
 
         #---------------------------------DYING---------------------------------
         # if self._current_state == self.states['Dying']:
@@ -260,6 +314,7 @@ class RL_agent(AiPlayer):
             case 'RIGHT':
                 self._move_right()
             case 'PLACE_BOMB':
+                self._stop_move()
                 self.place_bomb()
             case 'IDLE':
                 self._stop_move()
@@ -274,32 +329,39 @@ class RL_agent(AiPlayer):
         current_time = time.time()
 
         future_state = self.__get_state()
+        choosed_method = ""
         if current_time - self.last_decision_time >= 0.1:
             random_n = random.random()
             if random_n < self.table.epsilon:
-                if self.table.epsilon > 0.4:
-                    if self._can_place_bomb():
-                        selected_action = random.choice(self.actions)
-                        print("\033[91mRANDOM CHOICE\033[0m")
-                    else:
-                        selected_action = random.choice(self.actions[:-1])
-                        print("\033[91mRANDOM CHOICE WITHOUTH BOMB\033[0m")
-                else:
-                    selected_action = random.choice(self.actions[:-1])
-                    print("\033[91mRANDOM CHOICE WITHOUTH BOMB\033[0m")
-            else:
+                # if self.table.epsilon > 0.4:
                 if self._can_place_bomb():
-                    max_action_index = np.argmax(self.table.get(future_state))
-                    print("\033[94mGREEDY CHOICE\033[0m")
+                    selected_action = random.choice(self.actions)
+                    choosed_method = "\033[91mRANDOM CHOICE\033[0m"
                 else:
-                    max_action_index = np.argmax(self.table.get(future_state)[:-1])
-                    print("\033[94mGREEDY CHOICE WITHOUTH BOMB\033[0m")
+                    act = self.actions[:-1]
+                    selected_action = random.choice(act)
+                    choosed_method = "\033[91mRANDOM CHOICE WITHOUTH BOMB\033[0m"
+                # else:
+                #     selected_action = random.choice(self.actions[:-1])
+                #     print("\033[91mRANDOM CHOICE UNDER 0.4 epsilon\033[0m")
+            else:
+                f_state = []
+                if self._can_place_bomb():
+                    f_state = self.table.get(future_state)
+                    max_action_index = np.argmax(f_state)
+                    choosed_method = "\033[94mGREEDY CHOICE\033[0m"
+                else:
+                    f_state = self.table.get(future_state)[:-1]
+                    max_action_index = np.argmax(f_state)
+                    choosed_method = "\033[94mGREEDY CHOICE WITHOUTH BOMB\033[0m"
                 value = self.table.get(future_state)[max_action_index]
-                random_actions = list(filter(lambda index: value - 1 <=self.table.get(future_state)[index], range(len(self.table.get(future_state)))))
+                random_actions = list(filter(lambda index: value - 1 <=f_state[index], range(len(f_state))))
                 max_action_index = random.choice(random_actions)
                 selected_action = self.actions[max_action_index]
 
             next_max = np.max(self.table.get(future_state))
+            print(self.__last_choice_method)
+            # print(choosed_method)
 
             if self.performed_action is not None:
                 reward = self.__get_reward()   
@@ -330,6 +392,7 @@ class RL_agent(AiPlayer):
             self.last_decision_time = current_time
             self.performed_action = selected_action
             self.performed_state = future_state
+            self.__last_choice_method = choosed_method
 
             if self.table.epsilon > 0.4:
                 self.table.epsilon -= self.table.epsilon * 0.0001
